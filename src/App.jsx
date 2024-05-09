976e1b2ad748ae26154867ec2d55c05647a81d6f
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-09 09:06:11 +0530

    feat: useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import instance from "./AxiosInstance";
+import { useState } from "react";
+import { useReducer } from "react";
 
 const App = () => {
-  async function apiCallUsingAxios() {
-    let data=await instance({ method: "GET",timeout:1000 });
-    console.log(data)
-  }
-  apiCallUsingAxios()
-  return <div></div>;
+  const reducerfunc = (state, action) => {
+   switch(action.type)
+   {
+    case "email":
+      return {...state,email:action.value}
+   }
+         
+  
+  };
+  const [state, dispatch] = useReducer(reducerfunc, {
+    email: "",
+    password: "",
+  });
+
+  const handleEmailChange = (e) => {
+    dispatch({ type: "email", value: e.target.value });
+  };
+  const handlePassChange = () => {};
+  return (
+    <div>
+      email:{state.email}
+      <input className="border border-black" name="email" onChange={handleEmailChange} />
+      <input  className="border border-black"  name="password" onChange={handlePassChange} />
+    </div>
+  );
 };
 
 export default App;
--- src/AxiosInstance.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const instance = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/users",
-  // timeout: 1000,
-});
-
-export default instance;
