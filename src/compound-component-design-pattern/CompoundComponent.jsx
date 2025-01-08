5f94d6d3ec4db7fd039a4389876a9ee39c5dc841
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-01-08 11:24:40 +0530

    sorry removed un necssary code from compound component

--- src/compound-component-design-pattern/CompoundComponent.jsx
+++ src/compound-component-design-pattern/CompoundComponent.jsx
@@ -19,31 +19,3 @@ const CompoundComponent = () => {
   )
 }
 export default CompoundComponent
-
-const Modal = ({children}) => {
-    return (
-        <>{children}</>
-    )
-}
-const Title = ({children}) => {
-    return (
-        <>{children}</>
-    )
-}
-const Body = ({children}) => {
-    return (
-        <>{children}</>
-    )
-}
-
-<Modal>
-    <Title>Lets Get started with Compound Component</Title>
-    <Body>
-        <ul>
-            <li>kirtikumar</li>
-            <li>kirtikumar</li>
-            <li>kirtikumar</li>
-            <li>kirtikumar</li>
-        </ul>
-    </Body>
-</Modal>
